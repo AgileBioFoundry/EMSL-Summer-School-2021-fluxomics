074029eda34dc64877e30f836f779b2e1e688503
Mark Borkum <mark.borkum@pnnl.gov>
2017-10-27 18:19:02 -0700

    Handle reversible reactions.

--- stoich.rb
+++ stoich.rb
@@ -9,6 +9,7 @@ data = ::File.open(::File.expand_path('../P_putida_model.json', __FILE__), 'r') 
 end
 
 metabolite_ids = []
+
 reaction_ids = []
 
 stoichiometry_matrix_column_major_sparse = {}
@@ -20,20 +21,46 @@ data['metabolites'].each do |hash0|
 end
 
 metabolite_ids.sort_by! { |metabolite_id|
-  metabolite_id.split('_').last
+  value = metabolite_id.split('_').last
+
+  [value, metabolite_id]
 }
 
 data['reactions'].each do |hash0|
   reaction_id = hash0['id']
   reaction_metabolites = hash0['metabolites']
+  reaction_reversibility = hash0['notes']['map_info']['reversibility']
 
-  reaction_ids << reaction_id
+  forward_reaction_id = '%s$f' % [reaction_id]
+
+  reaction_ids << forward_reaction_id
 
   reaction_metabolites.each do |metabolite_id, stoichiometric_coefficient|
-    stoichiometry_matrix_column_major_sparse[reaction_id] ||= {}
-    stoichiometry_matrix_column_major_sparse[reaction_id][metabolite_id] = stoichiometric_coefficient
+    stoichiometry_matrix_column_major_sparse[forward_reaction_id] ||= {}
+    stoichiometry_matrix_column_major_sparse[forward_reaction_id][metabolite_id] = stoichiometric_coefficient
+  end
+
+  if reaction_reversibility
+    backward_reaction_id = '%s$r' % [reaction_id]
+
+    reaction_ids << backward_reaction_id
+
+    reaction_metabolites.each do |metabolite_id, stoichiometric_coefficient|
+      stoichiometry_matrix_column_major_sparse[backward_reaction_id] ||= {}
+      stoichiometry_matrix_column_major_sparse[backward_reaction_id][metabolite_id] = -1 * stoichiometric_coefficient
+    end
   end
 end
+
+reaction_ids.sort_by! { |reaction_id|
+  value = case reaction_id.split('$').last
+    when 'r' then 0
+    when 'f' then 1
+    else 2
+  end
+  
+  [value, reaction_id]
+}
 
 stoichiometric_matrix_row_major_dense = ::Array.new(metabolite_ids.size) { |array| ::Array.new(reaction_ids.size, 0) }
 
